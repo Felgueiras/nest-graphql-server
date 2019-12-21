89768f0f8cf6375e415e8af64324bae87a8d48d8
Felgueiras <rafaelfelgueiras1993@gmail.com>
2019-12-21 21:15:19 +0000

    CRUD ok

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { RecipesModule } from './recipes/recipes.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: 'schema.gql',
+    }),
+    RecipesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/recipes/recipes.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { NewRecipeInput } from './dto/new-recipe.input';
+import { RecipesArgs } from './dto/recipes.args';
+import { Recipe } from './models/recipe';
+
+@Injectable()
+export class RecipesService {
+  /**
+   * MOCK
+   * Put some real business logic here
+   * Left for demonstration purposes
+   */
+
+  private readonly recipes: Recipe[] = [];
+
+  async create(data: NewRecipeInput): Promise<Recipe> {
+    const newRecipe = data as Recipe;
+    // set ID
+    newRecipe.id = Math.random() + '';
+    this.recipes.push(newRecipe);
+    return newRecipe;
+  }
+
+  async findOneById(id: string): Promise<Recipe> {
+    const foundRecipe = this.recipes.find(recipe => recipe.id === id);
+    if (!foundRecipe) {
+      throw new NotFoundException('Recipe was not found');
+    }
+    return foundRecipe;
+  }
+
+  async findAll(recipesArgs: RecipesArgs): Promise<Recipe[]> {
+    // TODO: process args
+    return this.recipes.slice();
+  }
+
+  async remove(id: string): Promise<boolean> {
+    // TODO: remove
+    const foundRecipe = this.recipes.findIndex(recipe => recipe.id === id);
+    if (foundRecipe === -1) {
+      throw new NotFoundException("Recipe doesn't exist");
+    }
+    const deleted = this.recipes.splice(foundRecipe, 1);
+    console.log(deleted, this.recipes);
+    return true;
+  }
+}
